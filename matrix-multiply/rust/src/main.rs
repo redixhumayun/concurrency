c80a8122b0a19a475c82fbe0ee8c981e62c7fa27
Zaid Humayun <zaidhumayun@Zaids-Laptop.local>
2023-12-28 15:25:30 +0530

    added matrix multiplication without initialising entire 2d vector in one shot. this seems to significantly speed up time

--- matrix-multiply/rust/src/main.rs
+++ matrix-multiply/rust/src/main.rs
@@ -1,4 +1,27 @@
+use std::time::Instant;
+
 use rand::Rng;
+
+fn matrix_multiply_with_no_buffer(a: &Vec<Vec<i32>>, b: &Vec<Vec<i32>>) -> Vec<Vec<i32>> {
+    let m = a.len();
+    let n = b.len();
+    let p = b[0].len();
+
+    let mut c = Vec::with_capacity(m); //  initialising the buffer for just m rows
+
+    for i in 0..m {
+        let mut c_row = Vec::with_capacity(p);
+        for j in 0..p {
+            let mut sum = 0;
+            for k in 0..n {
+                sum += a[i][k] * b[k][j];
+            }
+            c_row.push(sum);
+        }
+        c.push(c_row);
+    }
+    c
+}
 
 fn matrix_multiply(a: &Vec<Vec<i32>>, b: &Vec<Vec<i32>>) -> Vec<Vec<i32>> {
     let m = a.len();
@@ -19,27 +42,37 @@ fn matrix_multiply(a: &Vec<Vec<i32>>, b: &Vec<Vec<i32>>) -> Vec<Vec<i32>> {
 }
 
 fn main() {
-    let rows = 2;
-    let cols = 2;
-    let a: Vec<Vec<i32>> = (0..rows)
+    let rows_a = 1000;
+    let cols_a = 1000;
+
+    let rows_b = 1000;
+    let cols_b = 1000;
+
+    let a: Vec<Vec<i32>> = (0..rows_a)
         .map(|_| {
-            return (0..cols)
+            return (0..cols_a)
                 .map(|_| rand::thread_rng().gen_range(1..100))
                 .collect();
         })
         .collect();
 
-    let b: Vec<Vec<i32>> = (0..rows)
+    let b: Vec<Vec<i32>> = (0..rows_b)
         .map(|_| {
-            return (0..cols)
+            return (0..cols_b)
                 .map(|_| rand::thread_rng().gen_range(1..100))
                 .collect();
         })
         .collect();
 
-    println!("{:?}", a);
-    println!("{:?}", b);
-
+    let start = Instant::now();
     let c = matrix_multiply(&a, &b);
-    println!("{:?}", c);
+    let duration = start.elapsed();
+    println!("Time duration for sequential: {:?}", duration);
+
+    let start = Instant::now();
+    let c_unbuffered = matrix_multiply_with_no_buffer(&a, &b);
+    let duration = start.elapsed();
+    println!("Time duration for no buffer: {:?}", duration);
+
+    assert_eq!(c, c_unbuffered);
 }
