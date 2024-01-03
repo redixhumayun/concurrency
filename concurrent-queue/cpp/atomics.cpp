38225e055e9df9cb437f69928c08b0f3d0a7af30
Zaid Humayun <redixhumayun@gmail.com>
2024-01-03 13:19:37 +0530

    basic atomic flag example

--- concurrent-queue/cpp/atomics.cpp
+++ concurrent-queue/cpp/atomics.cpp
@@ -2,28 +2,42 @@
 #include <iostream>
 #include <thread>
 
-std::atomic<bool> ready(false);
-int data = 0;
+// std::atomic<bool> ready(false);
 
-void producer() {
-  data = 100;                                    // Write data
-  ready.store(true, std::memory_order_relaxed);  // Set flag
+void set_atomic_flag_values() {
+  std::atomic_flag flag = ATOMIC_FLAG_INIT;
+  bool x = flag.test_and_set();
+  std::cout << x << std::endl;
+  bool y = flag.test_and_set();
+  std::cout << y << std::endl;
 }
 
-void consumer() {
-  while (!ready.load(std::memory_order_seq_cst))
-    ;  // Wait for flag
-  std::cout << "data: " << data << std::endl;
-  if (data != 100) {  // Check data
-    std::cerr << "data is not 100!" << std::endl;
-    std::terminate();  // Fail if data is not as expected
-  }
-}
+int main() { set_atomic_flag_values(); }
 
-int main() {
-  std::thread t1(producer);
-  std::thread t2(consumer);
-  t1.join();
-  t2.join();
-  return 0;
-}
+// int data = 0;
+
+// void producer() {
+//   data = 100;  // Write data
+//   ready = true;
+//   // ready.store(true);  // Set flag
+// }
+
+// void consumer() {
+//   // while (!ready.load())
+//   //   ;  // Wait for flag
+//   while (!ready)
+//     ;
+//   std::cout << "data: " << data << std::endl;
+//   if (data != 100) {  // Check data
+//     std::cerr << "data is not 100!" << std::endl;
+//     std::terminate();  // Fail if data is not as expected
+//   }
+// }
+
+// int main() {
+//   std::thread t1(producer);
+//   std::thread t2(consumer);
+//   t1.join();
+//   t2.join();
+//   return 0;
+// }
